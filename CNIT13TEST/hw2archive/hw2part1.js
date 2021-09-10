1e0d82ce53734b9ac5a3de99249daa6d64bdf25d
racalayag <89223252+racalayag@users.noreply.github.com>
2021-09-10 16:06:23 -0700

    Add files via upload

---
+++ CNIT13TEST/hw2archive/hw2part1.js
@@ -0,0 +1,15 @@
+
+document.write("<span style = 'font-family:Times New Roman;color:red;font-style:bold;'>ab&nbsp;</span>");
+document.write("<span style = 'font-family:Arial;color:blue;font-style:bold;'>12&nbsp;</span>");
+document.write("<span style = 'font-family:Impact;color:green;font-style:bold;'><em>34</em></span>");
+
+// Create a web page that contains a script that displays in pairs and strong (bold), the letters ab and the numbers 12 and 34 on the same line, with each pair of characters separated by one space.
+
+// Write the script using document.write statements.
+
+// Please use
+
+//         font family of Times New Roman and the color red for the letters ab
+//         font family of Arial and the color blue for the the numbers 12
+//         font family of Impact, the color green, and emphasized (in italics) for the numbers 34.
+//         Your output should look like this:       ab 12 34
